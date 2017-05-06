5dbd73cd83fa08edb2baceed783e2e7fec37e89f
Ben Kolera <ben.kolera@ephox.com>
2017-05-06 22:12:00 +1000

    ex2.1

--- Main.elm
+++ Main.elm
@@ -4,48 +4,33 @@ import Html.Events as HE
 import Platform.Cmd exposing (Cmd)
 import Platform.Sub exposing (Sub)
 
--- This datatype describes the events that can happen in our UI.
-type Msg = Toggle | Reset
+type Msg = Noop
 
--- This is the model that drives the UI
-type alias Model = { on : Bool, clicks : Int }
+type alias Todo = 
+  { completed : Bool
+  , title     : String 
+  }
 
--- We initialise the model to our starting value
+type alias Model = 
+  { todos : List Todo }
+
 init : Model
-init = { on = True, clicks = 0 }
+init = { todos = 
+  [ { completed = False , title = "Write Talk" }
+  , { completed = True  , title = "Propose Talk" }
+  ]}
 
--- A function from model -> Html 
 view : Model -> H.Html Msg
-view model = 
-  let divBy5 = model.clicks % 5 == 0 
-      divBy3 = model.clicks % 3 == 0
-      msg = if divBy5 && divBy3 then "FizzBuzz"
-            else if divBy3 then "Fizz"
-            else if divBy5 then "Buzz"
-            else toString model.clicks
-  in H.body []
-    -- This button raises a toggle event that triggers an update
-    [ H.button [ HA.class "big-button", HE.onClick Toggle ] 
-      [H.text msg] ]
+view model = H.body [] []
 
--- Our update takes in our Msg and returns a new model (and optional side effect)
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
   case msg of
-    -- We pattern match our two cases for Msg and act accordingly
-    Toggle -> ( 
-      { model 
-      | on = not model.on
-      , clicks = model.clicks + 1 
-      }, Cmd.none ) -- Cmd.none just means no side effect.
-    Reset -> ( init, Cmd.none )
+    Noop -> ( model, Cmd.none )
 
--- Subscriptions are messages that are pushed in from the outside world.
--- Things like incoming websocket traffic, the current time, etc.
 subscriptions : Model -> Sub Msg
 subscriptions model = Sub.none
 
--- Wire everything together into our program.
 main : Program Never Model Msg
 main =
   H.program
